be44ed4124f50d2e487794eb258c58a0cbf77317
Ivan <ivan-kozlow04@mail.ru>
2024-01-18 20:58:53 +0300

    add env

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ import CheckAuth from './utils/CheckAuth.js'
 import { upload } from './controllers/filesController.js'
 
 mongoose
-	.connect('mongodb+srv://admin:rootroot@tododb.ztwhx3o.mongodb.net/ToDoDB?retryWrites=true&w=majority')
+	.connect(process.env.MONGODB_URI)
 	.then(() => console.log('DB ok'))
 	.catch((err) => console.log('Error to connect DB', err))
 
@@ -36,4 +36,4 @@ app.post('/todo/create', CheckAuth, todoCreateValidation, TodoController.create)
 app.patch('/todo/:id', CheckAuth, todoUpdateValidation, TodoController.update)
 app.delete('/todo/:id', CheckAuth, TodoController.remove)
 
-app.listen(4000, (err) => err && console.log(err))
+app.listen(process.env.PORT || 4000, (err) => err && console.log(err))
